01d8831376fbefde41c2b440853f0a044e117f69
Kofi Twum-Ampofo <pktwum96@gmail.com>
2024-11-28 22:04:14 +0000

    remove hidden input

--- src/pages/RequestMemePage/index.tsx
+++ src/pages/RequestMemePage/index.tsx
@@ -22,8 +22,6 @@ export const RequestMemePage = () => {
           gap: 2,
         }}
       >
-        {/* Hidden input for Netlify to identify the form */}
-        <input type="hidden" name="form-name" value="Meme Upload Requests" />
         <Typography variant="h6" gutterBottom>
           Request a Meme Upload
         </Typography>
